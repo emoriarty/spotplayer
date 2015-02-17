d8c0a38d43ead5e9fca252f092c6ded177bcaa14
Enrique Arias <enric@Gerardos-MacBook-Pro.local>
2015-02-17 18:46:28 +0100

    Organized module Spotify.

--- lib/spotify.rb
+++ lib/spotify.rb
@@ -10,64 +10,68 @@ require 'date'
 #
 module Spotify
   include HTTParty
+  
+  REFRESH_METHODS = [:me]
 
-  def self.before(*reject_names)
-    names = instance_methods.reject{ |name| not reject_names.include?(name) }
-    names.each do |name|
-      m = instance_method(name)
-      define_method(name) do |*args, &block|  
-        yield
-        m.bind(self).(*args, &block)
+  module ClassMethods
+    def pre_call(names = nil)
+      names = names ? names.concat(REFRESH_METHODS) : REFRESH_METHODS
+      names = instance_methods.reject{ |name| !names.include?(name) }
+      puts names.inspect
+      names.each do |name|
+        m = instance_method(name)
+        define_method(name) do |*args, &block|  
+          yield
+          m.bind(self).(*args, &block)
+        end
       end
     end
   end
   
-  def access_token(code, client_id, client_secret, log = nil)
-    res = post("https://accounts.spotify.com/api/token",
-      :body => {
-        :grant_type => "authorization_code",
-        :code => code,
-        :redirect_uri => "http://localhost:5000/auth/spotify/callback"
-      },
-      :headers => {
-        "Authorize": "Basic #{id_and_secret_encoded(client_id, client_secret)}"
-      },
-      :debug_output => log)
-      
-      yield res
-  end
+  module InstanceMethods
+    # Block argument
+    def access_token(code, client_id, client_secret, log = nil)
+      return nil unless block_given?    
+      yield post("https://accounts.spotify.com/api/token",
+        :body => {
+          :grant_type => "authorization_code",
+          :code => code,
+          :redirect_uri => "http://localhost:5000/auth/spotify/callback"
+        },
+        :headers => {
+          "Authorize": "Basic #{id_and_secret_encoded(client_id, client_secret)}"
+        },
+        :debug_output => log)
+    end
 
-  def refresh_token(token, client_id, client_secret, log)
-    yield post("https://accounts.spotify.com/api/token",
-      :body => {
-        :grant_type => "resfresh_token",
-        :refresh_token => token
-      },
-      :headers => {
-        "Authorize": "Basic #{id_and_secret_encoded(client_id, client_secret)}"
-      },
-      :debug_output => log)
-  end
+    def refresh_token(token, client_id, client_secret, log)
+      return nil unless block_given?    
+      yield post("https://accounts.spotify.com/api/token",
+        :body => {
+          :grant_type => "resfresh_token",
+          :refresh_token => token
+        },
+        :headers => {
+          "Authorize": "Basic #{id_and_secret_encoded(client_id, client_secret)}"
+        },
+        :debug_output => log)
+    end
 
-  def id_and_secret_encoded(client_id, client_secret)
-    Base64.strict_encode64("#{client_id}:#{client_secret}")
-  end
+    def id_and_secret_encoded(client_id, client_secret)
+      Base64.strict_encode64("#{client_id}:#{client_secret}")
+    end
 
-  def refresh_token?(expires_at)
-    expires_at <= DateTime.now
-  end
+    def refresh_token?(expires_at)
+      expires_at <= DateTime.now
+    end
 
-  def me(token)
-    HTTParty.get("https://api.spotify.com/v1/me",
-      headers: {
-        "Authorization" => "Bearer #{ token }"
-      })
-  end
-
-  before(:me) do
-    if refresh_token? expires_at
-      res = refresh_token token, client_id, client_secret
-      update_token res # This methods must exists in every class which includes this module
+    def me(token)
+      return nil unless block_given?    
+      yield get("https://api.spotify.com/v1/me",
+        headers: {
+          "Authorization" => "Bearer #{ token }"
+        })
     end
   end
+
 end
--- lib/user.rb
+++ lib/user.rb
@@ -1,8 +1,11 @@
 require 'data_mapper'
 require 'date'
+require_relative 'spotify'
 
 class User
   include DataMapper::Resource 
+  include Spotify::InstanceMethods
+  extend Spotify::ClassMethods
 
   property :id, Serial
   property :uid, String
@@ -22,6 +25,14 @@ class User
       false
     end
   end
+  
+  pre_call do
+    if refresh_token? expires_at
+      res = refresh_token token, client_id, client_secret
+      update_token res # This methods must exists in every class which includes this module
+    end
+  end
+
 end
 
 DataMapper.finalize
